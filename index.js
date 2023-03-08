cc5adcf1f28bdb9ee62cafe7bee02bbdeb7eb3e6
kimeiga <hak7alp@gmail.com>
2023-03-08 14:11:42 -0800

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+import LocomotiveScroll from "locomotive-scroll";
+// let LocomotiveScroll = require('./node_modules/locomotive-scroll/src/locomotive-scroll.js');
+
+const scroll = new LocomotiveScroll();
